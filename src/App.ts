c35d96b729b4190252efc2b4153460c8da8ec685
Quyen Schroeder <qschro01@student.ubc.ca>
2025-04-03 10:42:54 -0700

    updates App.ts to remove extraneous comments

--- src/App.ts
+++ src/App.ts
@@ -4,10 +4,8 @@ import RetrievalManager from "./RetrievalManager";
 
 async function main() {
     const documents = await RetrievalManager.fetchMeetingDocumentList();
-
-    // const data = await RetrievalManager.fetchAgenda(documents[2].agendaLink);
     const data = await RSSGenerator.writeFeed(documents);
-    fs.writeFile("out.rss", data, (err: any) => {
+    fs.writeFile("./out.rss", data, (err: any) => {
         // In case of a error throw err.
         if (err) throw err;
     });
